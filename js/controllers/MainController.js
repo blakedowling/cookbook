5fd0127b15792b0440011e7b0592f0fb0aa1dbdb
Blake Dowling <blakeddowling@gmail.com>
2016-11-20 06:13:31 +0000

    angular functioning on multiple pages

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,7 +7,7 @@ app.controller('MainController',
       	{
           name: 'buckeyes',
           price: 15,
-          pubdate: new Date('2014', '03', '08'),
+          description: 'Chocolate-covered peanut butter balls #GoBuckeyes',
           cover: '/../images/buckeyes.JPG',
           likes: 0,
           dislikes: 0
@@ -15,7 +15,7 @@ app.controller('MainController',
         {
           name: 'Pumpkin chocolate chip cookies',
           price: 10,
-          pubdate: new Date('2013', '08', '01'),
+          description: 'Good in the fall and good all year round',
           cover: '/../images/muffins.JPG',
           likes: 0,
           dislikes: 0
@@ -23,18 +23,44 @@ app.controller('MainController',
           {
             name: 'Caramel Apples',
             price: 5,
-            pubdate: new Date('1960', '6', '11'),
+            description: 'Especially good wehn watching a scary movie in October',
             cover: '/../images/caramelApple.JPG',
+            likes: 0,
+            dislikes: 0
+          }// },
+          // {
+          //   name: 'Cupcakes',
+          //   price: 4,
+          //   pubdate: new Date('2015', '10', '30'),
+          //   cover: '/../images/cupcakes.JPG',
+          //   likes: 0,
+          //   dislikes: 0
+          // }
+        ];
+        $scope.plusOne = function(index) {
+          $scope.desserts[index].likes += 1;
+        };
+        $scope.minusOne = function(index) {
+          $scope.desserts[index].dislikes += 1;
+        };
+        $scope.meals = 
+        [
+          {
+            name: 'Thanksgiving',
+            price: 20,
+            pubdate: new Date('1620', '11', '25'),
+            cover: '/../images/thanksgiving.JPG',
+            likes: 0,
+            dislikes: 0
+          }, {
+            name: 'Chicken Curry',
+            price: 9,
+            pubdate: new Date('2016', '11', '19'),
+            cover: '/../images/chickenCurry.JPG',
             likes: 0,
             dislikes: 0
           }
         ];
-        $scope.plusOne = function(index) {
-          $scope.products[index].likes += 1;
-        };
-        $scope.minusOne = function(index) {
-          $scope.products[index].dislikes += 1;
-        };
       }
     ]
   );
